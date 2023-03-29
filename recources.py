9d779c74f1c4e83878a79bcd7897a1c66f60da42
olstrike <oliverinacka@gmail.com>
2023-03-29 12:46:38 +0200

    Now its the actual working stuff

--- recources.py
+++ recources.py
@@ -1,12 +1,15 @@
+
 import requests
 
-url = "https://currency-exchange.p.rapidapi.com/listquotes"
+url = "https://currency-exchange.p.rapidapi.com/exchange"
+
+querystring = {"from":"SEK","to":"DKK","q":"1.0"}
 
 headers = {
-	"X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
+	"X-RapidAPI-Key": "201a289868msh0e86c97a5408242p168a1bjsn1d77b054a80b",
 	"X-RapidAPI-Host": "currency-exchange.p.rapidapi.com"
 }
 
-response = requests.request("GET", url, headers=headers)
+response = requests.request("GET", url, headers=headers, params=querystring)
 
 print(response.text)
